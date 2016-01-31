5294e336131f03c25c5c94a3bf79711bfc2be14a
Adithya Nagaraj-Tirumale <aditnaga@tank.soic.indiana.edu>
2016-01-31 17:40:59 -0500

    hough transform function created

--- a1.cpp
+++ a1.cpp
@@ -306,6 +306,10 @@ SDoublePlane find_edges(const SDoublePlane &input, double thresh=0)
 
 	return G;
 }
+SDoublePlane hough_transform(const SDoublePlane &edges)
+{
+return edges;
+}
 
 // Get Hamming distance map
 SDoublePlane get_Hamming_distance(const SDoublePlane &input, const SDoublePlane &target)
